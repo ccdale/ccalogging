1702fbae1a8a41cd099711a22ddc69ccde2e4aba
Chris Allison <chris.allison@bgch.co.uk>
2019-11-28 07:21:21 +0000

    move functionality into __init__.py

--- ccalogging/__init__.py
+++ ccalogging/__init__.py
@@ -17,6 +17,43 @@
 #     along with ccalogging.  If not, see <http://www.gnu.org/licenses/>.
 #
 """python module for easy logging"""
+import sys
+import logging
+
+def setDebug():
+    log.setLevel(logging.DEBUG)
+
+def setInfo():
+    log.setLevel(logging.INFO)
+
+def setWarn():
+    log.setLevel(logging.WARNING)
+
+def setError():
+    log.setLevel(logging.ERROR)
+
+def setLogFile(fqfn, fformat="%(asctime)s [%(levelname)-5.5s]  %(message)s", datefmt="%d/%m/%Y %H:%M:%S"):
+    """
+    sets log output to go to a file
+    """
+    ffmt = logging.Formatter(fformat, datefmt=datefmt)
+    fileH = logging.FileHandler(fqfn)
+    fileH.setFormatter(ffmt)
+    log.addHandler(fileH)
+
+def setConsoleOut(STDOUT=False, cformat="%(asctime)s [%(levelname)-5.5s]  %(message)s", datefmt="%d/%m/%Y %H:%M:%S"):
+    """
+    sets log output to goto the console (stderr by default)
+    """
+    cfmt = logging.Formatter(cformat, datefmt=datefmt)
+    if STDOUT:
+        consH = logging.StreamHandler(sys.stdout)
+    else:
+        consH = logging.StreamHandler(sys.stderr)
+    consH.setFormatter(cfmt)
+    log.addHandler(consH)
+
+log = logging.getLogger("ccalogging")
 majorv = 0
 minorv = 1
 buildv = 0
