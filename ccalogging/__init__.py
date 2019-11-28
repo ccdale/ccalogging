c595601120d23022bc65e26e0cd24ff3a11a536c
Chris Allison <chris.allison@bgch.co.uk>
2019-11-28 06:19:10 +0000

    initial setup

---
+++ ccalogging/__init__.py
@@ -0,0 +1,24 @@
+#
+# Copyright (c) 2018, Centrica Hive Ltd.
+#
+#     This file is part of ccalogging.
+#
+#     ccalogging is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     ccalogging is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with ccalogging.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""python module for easy logging"""
+majorv = 0
+minorv = 1
+buildv = 0
+__version__ = str(majorv) + "." + str(minorv) + "." + str(buildv)
+__version_info__ = [majorv, minorv, buildv]
